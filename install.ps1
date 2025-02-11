d803b10d0154cd8828ecc235afdc5609a95310e8
sdmeth <ds.eth@yandex.ru>
2025-02-11 12:40:08 +0300

    Update install script for windows

--- install.ps1
+++ install.ps1
@@ -1,3 +1,101 @@
-# powershell -ExecutionPolicy Bypass -NoProfile -Command "& {Invoke-Expression ([System.Text.Encoding]::UTF8.GetString((Invoke-WebRequest -Uri 'https://sdmeth.github.io/utils/install.ps1').Content))}"
+param (
+    [string]$SoftwareName
+)
 
-Write-Host "Hello world!"
+function Check-Params {
+    param (
+        [string]$SoftwareName
+    )
+
+    if (-not $SoftwareName) {
+        Write-Host "Software name must be provided as a parameter"
+        exit 1
+    }
+}
+
+function Check-OS {
+    if ($IsWindows -eq $false) {
+        Write-Host "This script only supports Windows"
+        exit 1
+    }
+}
+
+function Check-Dir {
+    param (
+        [string]$SoftwareName
+    )
+
+    if ((Get-ChildItem -Path .).Count -gt 0) {
+        Write-Host "The directory is not empty. Found files:"
+        Get-ChildItem -Path .
+        Write-Host "You need to install $SoftwareName in an empty directory"
+        exit 1
+    }
+}
+
+function Check-Git {
+    Write-Host "Checking Git"
+    if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
+        Write-Host "Git is not installed"
+        return $false
+    }
+    Write-Host "Git is already installed"
+    return $true
+}
+
+function Install-Git {
+    Write-Host "Installing Git"
+    if (Get-Command winget -ErrorAction SilentlyContinue) {
+        winget install --id Git.Git -e -h 2> $null
+    }
+    else {
+        choco install git -y
+    }
+}
+
+function Download-Software {
+    param (
+        [string]$SoftwareName
+    )
+
+    Write-Host "Downloading $SoftwareName"
+
+    $ReleaseData = Invoke-RestMethod -Uri "https://api.github.com/repos/askaer-solutions/$SoftwareName/releases/latest" -UseBasicParsing -ErrorAction SilentlyContinue
+
+    if ($ReleaseData -eq $null) {
+        Write-Host "Failed to get latest release of $SoftwareName"
+        exit 1
+    }
+
+    $DownloadUrl = $ReleaseData.assets | Where-Object { $_.name -match "windows" } | Select-Object -ExpandProperty browser_download_url
+
+    if (-not $DownloadUrl) {
+        Write-Host "Failed to get the download URL for $SoftwareName"
+        exit 1
+    }
+
+    $SoftwareFile = "$SoftwareName" + "_windows.exe"
+    Invoke-WebRequest -Uri $DownloadUrl -OutFile $SoftwareFile
+
+    Write-Host "Installation has been successfully completed"
+    Write-Host "Starting $SoftwareName"
+    Start-Process -FilePath ".\$SoftwareFile" -NoNewWindow
+}
+
+function Start-Installation {
+    param (
+        [string]$SoftwareName
+    )
+
+    Check-Params -SoftwareName $SoftwareName
+    Check-OS
+    Check-Dir -SoftwareName $SoftwareName
+
+    if (-not (Check-Git)) {
+        Install-Git
+    }
+
+    Download-Software -SoftwareName $SoftwareName
+}
+
+Start-Installation -SoftwareName $SoftwareName
